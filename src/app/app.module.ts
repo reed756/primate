48904944edb6e7cdc94d8225a32be3a1f6ec95b9
reedanj <james.r@unitaskr.com>
2023-07-22 11:07:59 +0100

    amend sign in form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FontAwesomeModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FontAwesomeModule, ReactiveFormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/sign-in/sign-in.page.ts
+++ src/app/sign-in/sign-in.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,13 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInPage implements OnInit {
 
-  constructor() { }
+  signInForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email, Validators.minLength(5), Validators.maxLength(50)]],
+      password: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]]
+    })
+  }
 
   ngOnInit() {
+
   }
 
   onClick() {
 
+  }
+
+  signIn() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.signInForm);
+    console.log(this.signInForm?.value);
   }
 
 }
